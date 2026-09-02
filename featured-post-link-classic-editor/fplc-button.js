72463d0707f4f912f602171539451adb3e335b5c
redcypressdesignsblog <redcypressdesignsblog@gmail.com>
2026-09-02 11:57:22 -0500

    Add Featured Post Link Classic Editor: fplc-button.js

---
+++ featured-post-link-classic-editor/fplc-button.js
@@ -0,0 +1,37 @@
+( function () {
+	'use strict';
+
+	tinymce.PluginManager.add( 'fplc_button', function ( editor ) {
+		editor.addButton( 'fplc_button', {
+			title: 'Insert Featured Post Link',
+			icon: 'icon dashicons-admin-links',
+			onclick: function () {
+				const postOptions = ( typeof FPLC_POSTS !== 'undefined' )
+					? FPLC_POSTS.map( function ( post ) {
+						return {
+							text: post.title + ' (ID: ' + post.id + ')',
+							value: String( post.id )
+						};
+					} )
+					: [ { text: 'No posts found', value: '0' } ];
+
+				editor.windowManager.open( {
+					title: 'Insert Featured Post Link',
+					body: [ {
+						type: 'listbox',
+						name: 'postid',
+						label: 'Select a Post',
+						values: postOptions
+					} ],
+					onsubmit: function ( event ) {
+						if ( event.data.postid && event.data.postid !== '0' ) {
+							editor.insertContent( '[featured_post_link id="' + event.data.postid + '"]' );
+						} else {
+							window.alert( 'Please select a post.' );
+						}
+					}
+				} );
+			}
+		} );
+	} );
+} )();
